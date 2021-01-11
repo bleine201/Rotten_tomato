a1d306b056f45775f6c4e1c6616b4d6502b774cd
marizona <marie.jacquier@epitech.eu>
2021-01-11 14:56:27 +0100

    schema

--- rotten_tomatoes/migrations/001-db.sql
+++ rotten_tomatoes/migrations/001-db.sql
@@ -3,9 +3,24 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT,
-    password TEXT
+    password TEXT,
+    admin INTEGER,
+    favorite_movies TEXT
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    summary TEXT,
+    genre INTEGER,
+    producor TEXT,
+    release INTEGER,
+    image TEXT,
+    rating INTEGER,
+    review TEXT
+
 );
 
 -- Down
 DROP TABLE users;
-
+DROP TABLE movies;
